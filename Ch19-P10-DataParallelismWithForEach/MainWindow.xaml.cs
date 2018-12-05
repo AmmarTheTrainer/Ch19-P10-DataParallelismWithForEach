e326628afa98a73f263eb654df17d76590e71503
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-05 12:07:21 +0500

    update

--- Ch19-P10-DataParallelismWithForEach/MainWindow.xaml.cs
+++ Ch19-P10-DataParallelismWithForEach/MainWindow.xaml.cs
@@ -11,7 +11,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
+
+// Be sure you have these namespaces!
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
 
 namespace Ch19_P10_DataParallelismWithForEach
 {
@@ -24,5 +30,33 @@ namespace Ch19_P10_DataParallelismWithForEach
         {
             InitializeComponent();
         }
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            // This will be updated shortly
+        }
+        private void cmdProcess_Click(object sender, EventArgs e)
+        {
+            ProcessFiles();
+        }
+        private void ProcessFiles()
+        {
+            // Load up all *.jpg files, and make a new folder for the modified data.
+            string[] files = Directory.GetFiles(@".\TestPictures", "*.jpg", SearchOption.
+            AllDirectories);
+            string newDir = @".\ModifiedPictures";
+            Directory.CreateDirectory(newDir);
+            // Process the image data in a blocking manner.
+            foreach (string currentFile in files)
+            {
+                string filename = Path.GetFileName(currentFile);
+                using (Bitmap bitmap = new Bitmap(currentFile))
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    bitmap.Save(Path.Combine(newDir, filename));
+                    // Print out the ID of the thread processing the current image.
+                    this.Title = $"Processing {fielname} on thread {Thread.CurrentThread.ManagedThreadId}";
+            }
+        }
     }
+}
 }
